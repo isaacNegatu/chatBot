a979ef89f8f8050c138d832e068ed61b80a24fc2
Glitch (mnstate-webhook-test) <none>
2018-02-06 16:14:09 +0000

    👽🐾 Checkpoint package.json:306548/232 server.js:306548/44 actions/index.js:306548/1

--- server.js
+++ server.js
@@ -5,6 +5,11 @@
 var express = require('express');
 var app = express();
 
+var db = require('monk')(process.env.MONGODB_URI);
+var messages = db.get('messages');
+
+var apiaiHandler = require('./actions');
+
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
@@ -14,6 +19,13 @@ app.use(express.static('public'));
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
+});
+
+// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
+app.post("/apiai", function (request, response) {
+  console.log(request.body);
+  messages.insert(request.body);
+  apiaiHandler(request, response, farconData);
 });
 
 // listen for requests :)
