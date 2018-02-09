33f569c56e360b856d51a9777855bc58431eaa6e
Glitch (flax-move) <none>
2018-02-09 01:18:00 +0000

    🚪🙈 Checkpoint .env:660361/2 actions/test/index.js:660361/6 server.js:660361/276 actions/index.js:660361/32

--- actions/index.js
+++ actions/index.js
@@ -4,7 +4,7 @@ module.exports = function(req, res) {
   let action = req.body.result.action;
   
   // Test actions:
-  if (action.startsWith('test')) {
+  if (action.startsWith('input')) {
     testHandler(req, res);
   }
   
--- server.js
+++ server.js
@@ -9,7 +9,7 @@ let bodyParser = require('body-parser');
 // Create a simple connection to the MONGODB database
 // to store messages.
 let db = require('monk')(process.env.MONGODB_URI);
-let messages = db.get('messages');
+let messages = db.get('whatPeopleSaid');
 
 
 
@@ -19,10 +19,12 @@ let actionHandler = require('./actions');
 app.use(bodyParser.json());
 
 // The API endpoint for the requests from Dialog Flow
-app.post("/action", function (request, response) {
-  console.log(request.body);
-  messages.insert(request.body); // Save message in database
-  actionHandler(request, response);
+app.post("/action", function (req, res) {
+  console.log(req.body);
+  messages.insert(req.body);
+  // Save message in database
+  actionHandler(req, res);
+  
 });
 
 // listen for requests :)
