1c881dd2c8014466c8d31db9e18bdd81354f8705
Glitch (flax-move) <none>
2018-03-05 23:23:38 +0000

    🎬⛱ Checkpoint model/course.js:660361/168

--- model/course.js
+++ model/course.js
@@ -2,36 +2,30 @@ let mongoose = require('mongoose');
 
 var CourseSchema = new mongoose.Schema({
   _id : mongoose.Schema.Types.ObjectId,
+  college: String,
   title: String, 
-  courseID: { // Check with team
-    schoolID: String,
-    stateID: String,
-  },
+  courseID: Number,
   credits: Number,
   subject: String, // Math, Science, etc
-  section: String,
+  section: Number,
   description: String,
   location: String,
   room: String,
   seatAvailability: {
-    open: String,
-    size: String,
-    enrolled: String,
-    remainingSeats: String,
+    open: Boolean,
+    size: Number,
+    enrolled: Number,
+    remainingSeats: Number,
   },
   schedule: {  /// Days/Hours check with team
-    days: [{
-      day: {
-      	name: String,
-        time: {
-        	Start: String,
-          End: String,
-        },
-      }
-    }],
+    days: String,
+    time: String
   },
-  cost: Number
-
+  costs: {
+    fees: Number,
+    costResident: Number,
+    costNonResident: Number
+  }
 });
 
 module.exports = mongoose.model('Course', CourseSchema);
