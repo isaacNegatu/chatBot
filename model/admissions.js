3b0f5cc83f6454e4bc64d47e08136e7b57677bc8
Glitch (flax-move) <none>
2018-03-06 20:02:46 +0000

    🐠🖇 Checkpoint model/admissions.js:685186/370

--- model/admissions.js
+++ model/admissions.js
@@ -20,20 +20,97 @@ var admissionsschema = new Schema ({
       // How to get involved...
     },
     
-    stepsToApply: String, // <== Description or maybe add more
+    stepsToApply: {
+      description: String,
+      sumbittingTranscripts: String,
+      applicationLink: String,
+    },
     
     contactInformation: {
-      
       phone: String,
-      
+      fax: String,
+      //General Email
     }
   },
   
+  transferStudent: {
+    orientationTimes: [{
+      date: String,
+      time: String,
+    }],
+    
+    creditTransferPolicy: String,
+    
+    afterAcceptance: {
+      Description: String,
+      // More information regarding what is next
+      // How to get involved...
+    },
+    
+    stepsToApply: {
+      description: String,
+      sumbittingTranscripts: String,
+      applicationLink: String,
+    },
+    
+    contactInformation: {
+      phone: String,
+      fax: String,
+      //General Email
+    }
+  },
   
-  // Transfering Student
+  InternationStudent: {
+    orientationTimes: [{
+      date: String,
+      time: String,
+    }],
+    
+    afterAcceptance: {
+      Description: String,
+      // More information regarding what is next
+      // How to get involved...
+    },
+    
+    stepsToApply: {
+      description: String,
+      sumbittingTranscripts: String, // Internation transcripts?
+      applicationLink: String,
+    },
+    
+    contactInformation: {
+      phone: String,
+      fax: String,
+      //General Email
+    }
+  },
   
-  // International Student
-  
+  returningStudent: {
+    orientationTimes: [{
+      date: String,
+      time: String,
+    }],
+    
+    creditTransferPolicy: String,
+    
+    afterAcceptance: {
+      Description: String,
+      // More information regarding what is next
+      // How to get involved...
+    },
+    
+    stepsToApply: {
+      description: String,
+      sumbittingTranscripts: String,
+      applicationLink: String,
+    },
+    
+    contactInformation: {
+      phone: String,
+      fax: String,
+      //General Email
+    }
+  },
   // Returning Student
   
   // Highschool Options
