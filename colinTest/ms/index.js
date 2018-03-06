33143ba1d86abcea147420c3d9d680ac6acff6bb
Glitch (flax-move) <none>
2018-03-06 04:33:38 +0000

    👹🛫 Checkpoint colinTest/ms/index.js:487916/462:660361/124

--- colinTest/ms/index.js
+++ colinTest/ms/index.js
@@ -24,35 +24,76 @@ mongoose.connection.on('open', function (){
 
 
 
-function searchDBforCourse(course){
-  // console.log("here", course);
-  var reply = "";
-   
-  Course.find({subject: course}, function(err, course)
+const searchDBforCourse = new Promise((res, rej) => {
+   // console.log("here", course);
+   var reply = "";
+   Course.find({subject: course}, function(err, course)
       {
        if (err)
        {
          console.log(err);
+         rej(err);
        }else{
          reply = "yes it exits";
-         console.log(course);
+         console.log(course[0]);
        }
-
+  
     });
+  
+  res(reply);
    
-  console.log(reply, "ldkjfa");
-}
+  
+});
 
 
 
 module.exports = function(req, res) {
   let action = req.body.result.action;
   
+ 
   // console.log(req.body);
   var actionCommand = action.split('.');
   
   if (actionCommand[1] == "program" && actionCommand[2] == "exist"){
+    var course = req.body.result.parameters.courses;
+  
+    var reply = "";
     console.log(req.body.result.parameters);
-    searchDBforCourse(req.body.result.parameters.courses);
+   
+    console.log(reply, "then");
+   
   }
 }
+
+
+
+
+
+// (async function example() {
+//  let driver = await new Builder().forBrowser('firefox').build();
+//  try {
+//    await driver.get('https://eservices.minnstate.edu/registration/search/advanced.html?campusid=mnonline');
+//    //await driver.findElement(By.id('searchrcid')).sendKeys('webdriver', Key.RETURN);
+//    driver.getTitle().then(function(title) {
+//      console.log(title);
+
+//      if(title === 'Select a Campus') {
+//        console.log('Test passed');
+//      } else {
+//        console.log('Test failed');
+//      }
+//    });
+
+//    await driver.findElement(By.name('searchrcid')).findElement(By.tagName('option')).then(function (searchId){
+//      console.log("its woooooooooooooooooooooooooooooooooorking")
+//      console.log(searchId[1]);
+//    }).catch(err => {
+//      console.log(err);
+//    });
+
+//  } finally {
+//    await driver.quit();
+//  }
+// })().catch(err => {
+//   console.log(err);
+// });
