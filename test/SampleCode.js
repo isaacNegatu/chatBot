55e3f81f967719c6f86f24dfb3ce4956089d7b20
Glitch (flax-move) <none>
2018-03-06 20:50:51 +0000

    🗼📍 Checkpoint test/SampleCode.js:660361/21

--- test/SampleCode.js
+++ test/SampleCode.js
@@ -16,7 +16,7 @@ mongoose.connection
     mongoose.connection.db.listCollections().toArray(function(err, names) {
       if (err) console.log(err);
     });
-  }).then(function() {
+  }).then(async function() {
 
     testStaff.find({name: "Murphy, Marlin"}, function (err, doc){
       console.log(doc._id);
@@ -26,41 +26,41 @@ mongoose.connection
     var savedIDs = [];
     var count = 0;
 
-    // await Course.find({}, function (err, c){
-    //   c.forEach(function (doc){
-    //
-    //     testStaff.find({name: doc.instructor}, function(err, staff){
-    //       if (err) console.log(err);
-    //
-    //       console.log(staff.name, "d;kljfa");
-    //
-    //       if(staff.name != undefined){
-    //         flag = false;
-    //         console.log(staff.coursesTaught)
-    //
-    //         staff.coursesTaught.forEach(function(c){
-    //           savedIDs.push(c.coursesTaught._id);
-    //
-    //         });
-    //       }
-    //     }).then(function (){
-    //
-    //       savedIDs.push(doc._id);
-    //       if (!flag){
-    //         testStaff.findOneAndUpdate({name: doc.instructor}, {coursesTaught: savedIDs},function (err, doc){
-    //           if (err) console.log(err);
-    //           console.log("ids Updated")
-    //         });
-    //       }else{
-    //         var currentFaculty = new testStaff({
-    //           name: doc.instructor,
-    //           id: [doc._id]
-    //         });
-    //
-    //         currentFaculty.save();
-    //       }
-    //     });
-    //   });
-    //
-    // });
+    await Course.find({}, function (err, c){
+      c.forEach(function (doc){
+    
+        testStaff.find({name: doc.instructor}, function(err, staff){
+          if (err) console.log(err);
+    
+          console.log(staff.name, "d;kljfa");
+    
+          if(staff.name != undefined){
+            flag = false;
+            console.log(staff.coursesTaught)
+    
+            staff.coursesTaught.forEach(function(c){
+              savedIDs.push(c.coursesTaught._id);
+    
+            });
+          }
+        }).then(function (){
+    
+          savedIDs.push(doc._id);
+          if (!flag){
+            testStaff.findOneAndUpdate({name: doc.instructor}, {coursesTaught: savedIDs},function (err, doc){
+              if (err) console.log(err);
+              console.log("ids Updated")
+            });
+          }else{
+            var currentFaculty = new testStaff({
+              name: doc.instructor,
+              id: [doc._id]
+            });
+    
+            currentFaculty.save();
+          }
+        });
+      });
+    
+    });
   });
