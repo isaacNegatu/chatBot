5cef43b2fecdcff006cac5431e529d42c486cacf
Glitch (flax-move) <none>
2018-03-09 23:18:52 +0000

    🌮😄 Checkpoint actions/ms/department/offers/index.js:660361/349 actions/course/index.js:660361/2

--- actions/ms/department/offers/index.js
+++ actions/ms/department/offers/index.js
@@ -1,8 +1,6 @@
 
 let mongoose = require('mongoose');
-var Course = require("../../model/course");
-var CommonFees = require("../../model/commonFees");
-
+var Course = require("../../../../model/course");
 
 var db = process.env.MONGODB_URI;
  
@@ -20,3 +18,39 @@ mongoose.connection.on('open', function (){
   });
 
 });
+
+
+
+module.exports = async function(req, res) {
+  let action = req.body.result.action;
+  let reply = {};
+  
+  var course = req.body.result.parameters.course.toUpperCase();
+  var college = req.body.result.parameters.College;
+  
+  
+  await Course.findOne({subject: course, college: college}, function (err, c){
+    if (c.length !=0){
+      var str = `Yes, ` + college + ` offers ` + course + " classes";
+      reply = { 'speech' : str};
+    }else{
+      reply = {'speech' : `no we do not!`};
+    }
+  });
+  
+
+  
+  
+  
+  // Course.find({subject: actionArray[1]}, function(err, course)
+  //     {
+  //      if (err)
+  //      {
+  //        console.log(err);
+  //      }else{
+  //        reply = "yes it exits";
+  //      }
+  //   });
+    
+    res.status(200).json(reply);
+}
