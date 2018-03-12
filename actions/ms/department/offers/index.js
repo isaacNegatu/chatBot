856459d9454bc03ca38a9e16267cfaeb9679f4df
Glitch (flax-move) <none>
2018-03-12 04:34:22 +0000

    🛎👾 Checkpoint actions/ms/department/offers/index.js:660361/157 connection.js:660361/114 actions/ms/department/index.js:660361/1

--- actions/ms/department/offers/index.js
+++ actions/ms/department/offers/index.js
@@ -1,23 +1,8 @@
 
-let mongoose = require('mongoose');
 var Course = require("../../../../model/course");
 
-var db = process.env.MONGODB_URI;
- 
-mongoose.connect(db);
+let connection = require("../../../../connection");
 
-mongoose.connection.on('open', function (){
-  console.log("connected to mongo successfully");
-}).then(function (){
-  mongoose.connection.db.listCollections().toArray(function(err, names){
-    if (err) console.log(err);
-
-    names.forEach(function (col){
-      console.log(col.name);
-    });
-  });
-
-});
 
 module.exports = async function(req, res) {
   let action = req.body.result.action;
---
+++ connection.js
@@ -0,0 +1,18 @@
+var mongoose = require("mongoose");
+
+var db = process.env.MONGODB_URI;
+
+mongoose.connect(db);
+
+module.exports = mongoose.connection.on('open', function (){
+  console.log("connected to mongo successfully");
+}).then(function (){
+  mongoose.connection.db.listCollections().toArray(function(err, names){
+    if (err) console.log(err);
+
+    names.forEach(function (col){
+      console.log(col.name);
+    });
+  });
+
+});
