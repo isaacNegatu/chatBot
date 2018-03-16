e109564a5f42620850da45e6340c629c3ee9234e
Glitch (flax-move) <none>
2018-03-16 21:35:07 +0000

    🌠⚓️ Checkpoint ./question/index.js:660361/2695

--- question/index.js
+++ question/index.js
@@ -1,17 +1,34 @@
 var Question = require('../model/platformQuestions'); 
 
+var platform = "";
+var platform_id = "";
+var question = "";
 
 
 module.exports = function (req){ 
-  console.log(req.body);
-  var newQuestion = new Question({
-    platform: req.body.originalRequest.source,
-    id_fromPlatfrom: req.body.originalRequest.data.user,
-    date : new Date(),
-    question: req.body.result.resolvedQuery
+  
+  let getRequestInfo = function() {
+      return new Promise(function(resolve, reject) {
+        platform = req.body.originalRequest.source;
+        platform_id = req.body.originalRequest.data.user;
+        question = req.body.result.resolvedQuery
+      });
+    };
+  
+  getRequestInfo()
+  .then(function(){
+    
+    var newQuestion = new Question({
+      platform: platform,
+      id_fromPlatfrom: platform_id,
+      date : new Date(),
+      question: question
+    });
+  
+    newQuestion.save();
+    
   });
   
-  newQuestion.save();
 }
 
 
