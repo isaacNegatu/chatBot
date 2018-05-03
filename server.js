421dbaf91e056640f06452f469771b412926b625
Glitch (flax-move) <none>
2018-05-03 23:36:08 +0000

    🐆🌔 Checkpoint ./server.js:381538/25

--- server.js
+++ server.js
@@ -36,7 +36,9 @@ app.post("/action", function (req, res) {
   var td = function (){
    return req.header; 
   }
-    
+  
+  console.log(req);
+  
   if(req.body.originalRequest){
    questionHandler(req);
   }else{
