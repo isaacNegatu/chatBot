a3dd81ba89725c9b7af680e2f31adfe02eb79325
Glitch (flax-move) <none>
2018-05-28 23:27:21 +0000

    🚊🙆 Checkpoint ./actions/cc/courses/getCourse/byInstructorName/index.js:660361/1287

--- actions/cc/courses/getCourse/byInstructorName/index.js
+++ actions/cc/courses/getCourse/byInstructorName/index.js
@@ -10,8 +10,50 @@ module.exports = async function (req,res){
   
   console.log(req.body.result.parameters);
   
-  console.log("got to courses");
+  console.log("got to find by instructor");
   
+  let course = req.body.result.parameters.courses;
+  let fName = req.body.result.parameters.fName;
+  
+  console.log(course);
+  let lName = req.body.result.parameters.lName; 
+  
+  var fullName = `${lName},${fName}`;
+  
+  console.log(fullName);
+  
+  if (course == "" || course == undefined){
+      var str = fullName + " teaches : ";
+      var courseList = [];
+
+      await Faculty.findOne({name: fullName}).
+              populate("coursesTaught").
+              exec().
+              then(c => {
+                 
+               c.coursesTaught.forEach(function (d){
+                  let course = courseList.find(function (co){
+                    return co == d.title;
+                  });
+
+                  if(!course){
+                    courseList.push(d.title);
+                  }
+                  console.log(d.title);
+
+                });
+              })
+              .then(function (){
+                courseList.forEach(function (co){
+                  str += co + ", ";
+                })
+                var realStr = str.substr(0,str.length-2);
+                reply = {'speech' : realStr  + "."};
+
+              })
+              .catch(err => console.log(err));
+      }
+
   
   
 }
