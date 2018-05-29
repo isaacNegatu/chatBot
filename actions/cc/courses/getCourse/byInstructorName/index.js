b3495901caa9777f3199a5894e107c7bcadf4f0a
Glitch (flax-move) <none>
2018-05-29 01:20:40 +0000

    🚆🐷 Checkpoint ./actions/cc/courses/getCourse/byInstructorName/index.js:660361/1763

--- actions/cc/courses/getCourse/byInstructorName/index.js
+++ actions/cc/courses/getCourse/byInstructorName/index.js
@@ -119,7 +119,7 @@ module.exports = async function (req,res){
           .catch(err => console.log(err));
 
         
-      }else if (course != ""){
+      }else if (course != "" && term != ""){
         
         var str = `${fName} ${lName} teaches : `;
         var courseList = [];
@@ -168,6 +168,61 @@ module.exports = async function (req,res){
             reply = {'speech' : realStr  + "."};
           }else{
             reply = {'speech' : `${fName} ${lName} doesn't have a schedule in the ${term}`};
+          }
+
+          })
+          .catch(err => console.log(err));
+
+        
+      }else if (course != "" && term == ""){
+        
+        var str = `${fName} ${lName} teaches : `;
+        var courseList = [];
+
+        await Faculty.findOne({name: fullName}).
+          populate("coursesTaught").
+          exec().
+          then(c => {
+
+           c.coursesTaught.forEach(function (d){
+              
+              let course = courseList.find(function (co){
+                return co == d.courseID;
+              });
+
+              if(!course ){
+                courseList.push(d);
+              }
+              console.log(d.title , d.subject, d.number, d.section);
+
+            });
+          })
+          .then(function (){
+          
+            let flag = false;
+          
+            courseList.forEach(function (co){
+              let sub = co.subject;
+              let num = co.number;
+              let sec = co.section;
+              let tit = co.title;
+              let days = co.meetingDetails.days;
+              let time = co.meetingDetails.time;
+              
+              let termFromDB = co.semester.split(' ')[0];
+            
+              if(sub == course[0] && num == course[1]){
+                str += `${sub}-${num}.${sec} | ${tit} | ${days} | ${time} | ${termFromDB} , `; 
+                flag = true;
+              }
+            })
+          
+          if(flag){
+            
+            var realStr = str.substr(0,str.length-2);
+            reply = {'speech' : realStr  + "."};
+          }else{
+            reply = {'speech' : `${fName} ${lName} doesn't teach ${course[0]}-${course[1]}`};
           }
 
           })
