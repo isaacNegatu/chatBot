f40347daa23f5a336032f7eb4efc5aa6c9792d80
Glitch (flax-move) <none>
2018-05-29 01:41:43 +0000

    🌓🚣 Checkpoint ./actions/cc/courses/getCourse/byCourseID/index.js:660361/3390

--- actions/cc/courses/getCourse/byCourseID/index.js
+++ actions/cc/courses/getCourse/byCourseID/index.js
@@ -14,9 +14,63 @@ module.exports = async function (req, res){
   let term = req.body.result.parameters.term;
 
   if(term.length == 0){
-    let courseList == [];
+    let courseList = [];
+    let str = '';
    
-    await Course.find({ 
+    await Course.find({ subject : course[0] , number : course[1] }, function(err, docs){
+      
+      if (!docs.length){
+        reply = {'speech' : 'Course not Found' };
+      }else{
+        docs.forEach(function(co){
+          let sub = co.subject;
+          let num = co.number;
+          let sec = co.section;
+          let tit = co.title;
+          let instruct = co.meetingDetails.instructor;
+          let days = co.meetingDetails.days;
+          let time = co.meetingDetails.time;
+          let termFromDB = co.semester;
+
+          
+          str += `${sub}-${num}.${sec} | ${tit} | ${instruct} | ${days} | ${time} | ${termFromDB} , `; 
+          
+        });
+        var realStr = str.substr(0,str.length-2);
+        reply = {'speech' : realStr  + "."}; 
+      }
+      
+    });
     
-  }
+  }else{
+      let courseList = [];
+      let str = '';
+      let qTerm = term + ' 2018';
+      await Course.find({ subject : course[0] , number : course[1] , semester: qTerm}, function(err, docs){
+
+        if (!docs.length){
+          reply = {'speech' : 'Course not Found' };
+        }else{
+          docs.forEach(function(co){
+            let sub = co.subject;
+            let num = co.number;
+            let sec = co.section;
+            let tit = co.title;
+            let instruct = co.meetingDetails.instructor;
+            let days = co.meetingDetails.days;
+            let time = co.meetingDetails.time;
+            let termFromDB = co.semester;
+
+
+            str += `${sub}-${num}.${sec} | ${tit} | ${instruct} | ${days} | ${time} | ${termFromDB} , `; 
+
+          });
+          var realStr = str.substr(0,str.length-2);
+          reply = {'speech' : realStr  + "."}; 
+        }
+
+      });
+
+
+    }
 }
