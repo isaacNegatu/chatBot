75c83a835886fdb0114a389fad9a5c523f5f207a
Glitch (lime-mice) <none>
2018-05-31 23:13:59 +0000

    🎗🎆 Checkpoint ./server.js:660361/1174

--- server.js
+++ server.js
@@ -55,10 +55,10 @@ app.post("/action", function (req, res) {
   
   let reply = {"data" : {
                 "slack" : {
-                  "text": "Would you like to play a game?",
+                  "text": "Would you like to choose a semseter?",
                   "attachments": [
                       {
-                          "text": "Choose a game to play",
+                          
                           "fallback": "You are unable to choose a game",
                           "callback_id": "wopr_game",
                           "color": "#3AA3E3",
@@ -66,29 +66,23 @@ app.post("/action", function (req, res) {
                           "actions": [
                               {
                                   "name": "game",
-                                  "text": "Chess",
+                                  "text": "Fall",
                                   "type": "button",
                                   "value": "chess"
                               },
                               {
                                   "name": "game",
-                                  "text": "Falken's Maze",
+                                  "text": "Spring",
                                   "type": "button",
                                   "value": "maze"
                               },
                               {
                                   "name": "game",
-                                  "text": "Thermonuclear War",
-                                  "style": "danger",
+                                  "text": "Summer",
                                   "type": "button",
-                                  "value": "war",
-                                  "confirm": {
-                                      "title": "Are you sure?",
-                                      "text": "Wouldn't you prefer a good game of chess?",
-                                      "ok_text": "Yes",
-                                      "dismiss_text": "No"
-                                  }
+                                  "value": "maze"
                               }
+                              
                             ]
                         }
                     ]
