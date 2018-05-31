dc621fcf970d80707645e054440c8763b82425ef
Glitch (lime-mice) <none>
2018-05-31 01:03:23 +0000

    😋🛬 Checkpoint ./server.js:660361/16222

--- server.js
+++ server.js
@@ -19,6 +19,9 @@ let questionHandler = require('./question');
 
 // Use bodyParser to handle json
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'))
@@ -34,60 +37,53 @@ app.get("/", (request, response) => {
 // The API endpoint for the requests from Dialog Flow
 app.post("/action", function (req, res) {
   
-  console.log(req);
+  console.log(req.body.payload);
+  
+  if(req.body.payload)
   
   let reply = {"data" : {
                 "slack" : {
-                    "text": "New comic book alert!",
-                    "attachments": [
-                        {
-                            "title": "The Further Adventures of Slackbot",
-                            "fields": [
-                                {
-                                    "title": "Volume",
-                                    "value": "1",
-                                    "short": true
-                                },
-                                {
-                                    "title": "Issue",
-                                    "value": "3",
-                            "short": true
-                                }
-                            ],
-                            "author_name": "Stanford S. Strickland",
-                            "author_icon": "http://a.slack-edge.com/7f18https://a.slack-edge.com/bfaba/img/api/homepage_custom_integrations-2x.png",
-                            "image_url": "http://i.imgur.com/OJkaVOI.jpg?1"
-                        },
-                        {
-                            "title": "Synopsis",
-                            "text": "After @episod pushed exciting changes to a devious new branch back in Issue 1, Slackbot notifies @don about an unexpected deploy..."
-                        },
-                        {
-                            "fallback": "Would you recommend it to customers?",
-                            "title": "Would you recommend it to customers?",
-                            "callback_id": "comic_1234_xyz",
-                            "color": "#3AA3E3",
-                            "attachment_type": "default",
-                            "actions": [
-                                {
-                                    "name": "recommend",
-                                    "text": "Recommend",
-                                    "type": "button",
-                                    "value": "recommend"
-                                },
-                                {
-                                    "name": "no",
-                                    "text": "No",
-                                    "type": "button",
-                                    "value": "bad"
-                                }
+                  "text": "Would you like to play a game?",
+                  "attachments": [
+                      {
+                          "text": "Choose a game to play",
+                          "fallback": "You are unable to choose a game",
+                          "callback_id": "wopr_game",
+                          "color": "#3AA3E3",
+                          "attachment_type": "default",
+                          "actions": [
+                              {
+                                  "name": "game",
+                                  "text": "Chess",
+                                  "type": "button",
+                                  "value": "chess"
+                              },
+                              {
+                                  "name": "game",
+                                  "text": "Falken's Maze",
+                                  "type": "button",
+                                  "value": "maze"
+                              },
+                              {
+                                  "name": "game",
+                                  "text": "Thermonuclear War",
+                                  "style": "danger",
+                                  "type": "button",
+                                  "value": "war",
+                                  "confirm": {
+                                      "title": "Are you sure?",
+                                      "text": "Wouldn't you prefer a good game of chess?",
+                                      "ok_text": "Yes",
+                                      "dismiss_text": "No"
+                                  }
+                              }
                             ]
                         }
                     ]
                 }
               },
                "speech" : "hello"
-              };
+            };
   
   res.status(200).json(reply);
     
