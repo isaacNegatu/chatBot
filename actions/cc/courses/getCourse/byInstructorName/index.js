d522bd9ab90aae86d6522b1f076c178ef510cd40
Glitch (lime-mice) <none>
2018-06-01 00:27:35 +0000

    💒🐪 Checkpoint ./server.js:660361/159 ./actions/cc/courses/getCourse/byInstructorName/index.js:660361/3

--- actions/cc/courses/getCourse/byInstructorName/index.js
+++ actions/cc/courses/getCourse/byInstructorName/index.js
@@ -228,7 +228,7 @@ module.exports = async function (req,res){
       }
   
       //you need to build urls for all the available options
-      // res.status(200).json(reply);
+      res.status(200).json(reply);
       console.log('------------------------------------------------------------------------------------------------------------------');
       // console.log(res.json());
 
