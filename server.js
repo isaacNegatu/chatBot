487e3b2e3dc6d26de19e73ebe8aea14f3cad6afc
Glitch (lime-mice) <none>
2018-06-03 23:57:18 +0000

    🚵👮 Checkpoint ./server.js:660361/4536

--- server.js
+++ server.js
@@ -65,31 +65,23 @@ app.post("/action", function (req, res) {
   let reply = {"data" : 
                {
                 "facebook" : {
-                  "attachment":{
-                    "type":"template",
-                    "payload":{
-                      "template_type":"button",
-                      "text":"Please choose the semester",
-                      "buttons":[
-                        {
-                          "type":"postback",
-                          "title":"fall",
-                          "payload":`${fname} ${lname} fall?`
-                        },
-                         {
-                          "type":"postback",
-                          "title":"spring",
-                          "payload":`${fname} ${lname} spring?`
-                        },
-                         {
-                          "type":"postback",
-                          "title":"summer",
-                          "payload":`${fname} ${lname} summer?`
-                        }
-                      
-                      ]
-                    }
+                  
+                  "message":{
+                    "text":"Pick a color:",
+                    "quick_replies":[
+                      {
+                        "content_type":"text",
+                        "title":"Red",
+                        "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED"
+                      },
+                      {
+                        "content_type":"text",
+                        "title":"Green",
+                        "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_GREEN"
+                      }
+                    ]
                   }
+
                 }
                 
               },
