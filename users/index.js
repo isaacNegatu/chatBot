ec57daf8a7c8d3eaac0173e6c7a43bd95d8c6fc5
Glitch (lime-mice) <none>
2018-06-22 20:22:03 +0000

    🐸📊 Checkpoint ./users/index.js:660361/333

--- users/index.js
+++ users/index.js
@@ -1,32 +1,37 @@
-let User = require('../model/user');
+// let User = require('../model/user');
 
-let userID = '';
-let name = '';
+// let userID = '';
+// let name = '';
 
-module.exports = (req) =>{
+// let users = [];
+
+// module.exports = (req) =>{
   
-  let getUserInfo = ()=>{
-    return new Promise((res, rej) =>{
+//   let getUserInfo = ()=>{
+//     return new Promise((res, rej) =>{
       
-      userID = req.body.originalRequest.data.sender.id;
-    });
+//       userID = req.body.originalRequest.data.sender.id;
+//     });
 
-  }
+//   }
   
-  let searchIfExists = ()=>{
-    
-    
-    
-    
-  }
+//   let getUsers = ()=>{
+//     let query = User.find({});
+//     return query;
+//   }
+  
+//   getUsers().exec((err, docs)=>{
+//     if(err) return err;
+      
+//   });
   
   
-  getUserInfo()
-  .then(()=>{
+//   getUserInfo()
+//   .then(()=>{
     
-    let newUser = new User
+//     let newUser = new User
   
-  });
+//   });
   
-}
+// }
 
